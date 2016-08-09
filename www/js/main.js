2c21e8e397bbac58112bf724a952bb5759a4d0d8
Andre Christoga <andrechristoga@gmail.com>
2016-08-09 13:43:30 +0700

    Onlick, alert

--- www/js/main.js
+++ www/js/main.js
@@ -1,9 +1,10 @@
 window.onload = loadData();
-var ref = new Firebase('https://dr-iqbal.firebaseio.com');
 
 function loadData() {	
+	var ref = new Firebase('https://dr-iqbal.firebaseio.com');
 	ref.on('value', function(snapshot){
 		data = snapshot.val().no_antrian;
-		document.write.getElementById('jumlah').innerHTML = data;
+		// document.write.getElementById('jumlah').innerHTML = data;
+		alert(data)
 	}); 
 }
