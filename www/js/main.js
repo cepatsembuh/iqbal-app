f8a758ef09d192ce3e9c69c6cddcec3c3aa57733
Andre Christoga <andrechristoga@gmail.com>
2016-08-09 14:56:35 +0700

    Realtime data content

--- www/js/main.js
+++ www/js/main.js
@@ -3,7 +3,9 @@ var ref = new Firebase('https://dr-iqbal.firebaseio.com');
 
 function loadData() {	
 	ref.on('value', function(snapshot){
-		data = snapshot.val().no_antrian;
-		alert(data)
+		var data = snapshot.val().no_antrian,
+		label = document.getElementById('jumlah');
+
+		label.innerHTML = data
 	}); 
 }
