f5714969ff8112d40e116443d573044cbaf815e8
Andre Christoga <andrechristoga@gmail.com>
2016-08-09 14:54:10 +0700

    Clean code.

--- www/js/main.js
+++ www/js/main.js
@@ -1,10 +1,9 @@
 window.onload = loadData();
+var ref = new Firebase('https://dr-iqbal.firebaseio.com');
 
 function loadData() {	
-	var ref = new Firebase('https://dr-iqbal.firebaseio.com');
 	ref.on('value', function(snapshot){
 		data = snapshot.val().no_antrian;
-		// document.write.getElementById('jumlah').innerHTML = data;
 		alert(data)
 	}); 
 }
